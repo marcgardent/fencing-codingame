d565a240af0335247d11fbf4cf4020cdfe2fa683
marcgardent <marcgardent@yahoo.fr>
2021-01-04 17:13:44 +0100

    new doc, fixed, generated from markdown2cg!

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,13 @@
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
 
-import java.text.MessageFormat;
-
 public class Main {
     public static void main(String[] args) {
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
         int league = 2;
-        gameRunner.addAgent(MessageFormat.format("D:\\tmp\\cg-rust-playground\\dist\\artifacts\\league1.exe ", league));
-        gameRunner.addAgent(MessageFormat.format("D:\\tmp\\cg-rust-playground\\dist\\artifacts\\league{0}.exe ", league));
+        //gameRunner.addAgent(MessageFormat.format("D:\\tmp\\cg-rust-playground\\dist\\artifacts\\league1.exe ", league));
+        //gameRunner.addAgent(MessageFormat.format("D:\\tmp\\cg-rust-playground\\dist\\artifacts\\league{0}.exe ", league));
+        gameRunner.addAgent(Player.class);
+        gameRunner.addAgent(Player2.class);
         gameRunner.setLeagueLevel(league);
         gameRunner.start();
     }
